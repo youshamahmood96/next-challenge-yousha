7d56460e8f08c74859f56f5763b03cd317c7d4de
youshamahmood <youshamahmood96@gmail.com>
2021-01-30 01:38:21 +0600

    ok

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef5aab485334467d44c70009af0d2597'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1ae6ab7e-dd49-4543-a5a0-8a91ed07f77b'
+};
